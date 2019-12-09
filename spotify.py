38200386769354acb2f31aab6d57da4e962151b0
sadhana-sr <54877349+sadhana-sr@users.noreply.github.com>
2019-12-09 18:30:52 -0500

    Add files via upload

--- spotify.py
+++ spotify.py
@@ -5,6 +5,7 @@ import os
 import spotipy
 import sys
 import spotipy.util as util
+import sqlite3
 
 
 token = util.oauth2.SpotifyClientCredentials(client_id = '1ad705cfee3548b9950efb26b457a33e', client_secret = '3a4c9fe4f00542d9816b25148a035664')
@@ -19,7 +20,7 @@ scope = 'user-read-private'
 # Your name: Sadhana Ramaseshadri
 # Who you worked with:
 # OAuth Token: BQBtKFbRuaS9serMaqntRrYdrML59kodnuCCsdNyjLg5LHrJudK9Gs6wFz1BLWOtJO5_IEb02hIfH8oYwg5ulXOpxnZCCVYDP8XFbujAPPzKJgiuyQOZzAzkP4mQPOj-0X2UBDKLcSYk7tT67Hew6l0eg8n0Gq4BpEhVROBY0a-wtmmTRjHtROLcioyNfI5g5FoROkYOI5smU6dWHBiijFSiuACBmEQCjDoORgpG_Ny42p5K2dDsAf9aA90xd7MG2S-pVh1Xpw
-
+# API Key RandomWords: 3f40650948mshfb87ef8f6e80f48p16ef22jsnf380814b03c7
 url = "https://api.spotify.com/v1/search"
 scope = 'user-library-read'
 
@@ -54,7 +55,14 @@ def write_cache(CACHE_FNAME, cache_dict):
     print(cf)
 
 
-def get_songs_for_cache(keyword):
+#function that uses api to get key word
+#call it once, get word, plug in, 
+
+#put artist into artist table
+#song table plugin id and title
+# if sia ID is 2 in song table, then you can trace it back to song table, and then get artist
+
+def get_songs(keyword):
     
     if len(sys.argv) > 1:
         search_str = sys.argv[1]
@@ -68,7 +76,7 @@ def get_songs_for_cache(keyword):
         titles.append(item['name'])
     return titles
 
-def get_artists_for_cache(keyword):
+def get_artists(keyword):
     
     if len(sys.argv) > 1:
         search_str = sys.argv[1]
@@ -82,6 +90,31 @@ def get_artists_for_cache(keyword):
         artists.append(item['artists'][0]['name'])
     return artists
 
+def write_title_table(db_filename):
+    
+    conn = sqlite3.connect(db_filename)
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS Spotify (Title TEXT, Artist TEXT, artist_id INT)")
+
+
+def insert_title_table(db_filename):
+    conn = sqlite3.connect(db_filename)
+    cur = conn.cursor()
+    songs = get_songs('word')
+
+    for song in songs:
+        cur.execute("INSERT INTO Spotify(Title) VALUES(?)",song)
+    conn.commit()
+    
+
+
+def write_id_table(db_filename):
+    
+    conn = sqlite3.connect(db_filename)
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS Artists (Artist TEXT, artist_id INT)")
+ 
+#function that updates table with 20
 
 #------------------------------------------------------------------------------------------
 
@@ -89,9 +122,8 @@ def main():
     
     # Get the cached data for BRA
     print("This should use the cache")
-    songs = get_songs_for_cache('stupid')
-    artists = get_artists_for_cache('stupid')
-
+    songs = get_songs('word')
+    artists = get_artists('word')
     print(songs)
     print(artists)
     
@@ -102,7 +134,10 @@ def main():
     cache_dictionary = read_cache(CACHE_FNAME)
     write_cache(CACHE_FNAME, songs)
     write_cache(CACHE_FNAME, artists)
- 
+
+    write_title_table('songs.db')
+    insert_title_table('songs.db')
+    write_id_table('songs.db')
  
 
     # You can comment this out to test with just the main, but be sure to uncomment it and test
